231d9222b9e097482a217cb3fda3e519a22791ad
[BK] trueBlue <anhdd.101197@gmail.com>
2023-03-20 22:37:00 +0700

    test redux saga

--- src/app/rootSaga.ts
+++ src/app/rootSaga.ts
@@ -1,10 +1,20 @@
-import { all } from 'redux-saga/effects'
+import { PayloadAction } from '@reduxjs/toolkit';
+import { all, takeEvery } from 'redux-saga/effects';
 
 function* helloSaga() {
-    console.log('Hello Sagas!')
-  }
+  console.log('Hello Sagas!');
+}
+
+function* log(action: PayloadAction) {
+  console.log('Log', action);
+}
+
+function* countSaga() {
+  console.log('count Sagas!');
+  yield takeEvery('*', log);
+}
 
 export default function* rootSaga() {
-    console.log('root Saga');
-    yield all([helloSaga()])
+  console.log('root Saga');
+  yield all([helloSaga(), countSaga()]);
 }
